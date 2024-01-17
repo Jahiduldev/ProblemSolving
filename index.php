4840207a4586e3166b75cabf703b67a8fdc623c4
Jahidul Islam <jahiduldev@gmail.com>
2024-01-17 12:22:50 +0600

    problem1207

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function uniqueOccurrences($arr)
+    {
+        $counts = array_count_values($arr);
+        $arr1 =   array_values($counts);
+        $arr2 =   array_unique($arr1);
+        return count($arr1) === count($arr2);
+    }
+}
