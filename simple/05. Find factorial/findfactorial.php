bc94617dac7ab4b540a8521b31ab6d7c7c111c6a
Jahidul Islam <jahidulislam@Jahiduls-MacBook-Pro.local>
2024-03-06 19:47:31 +0600

    print pyramid and reverse pyramid pattern

---
+++ simple/05. Find factorial/findfactorial.php
@@ -0,0 +1,12 @@
+<?php
+function factorial($n)
+{
+    if($n <= 1)
+    {
+        return 1;
+    }
+    return $n * factorial($n - 1);
+}
+$n = 5;
+echo "Factorial of $n is " .factorial($n); /*Call the Function */
+?>
